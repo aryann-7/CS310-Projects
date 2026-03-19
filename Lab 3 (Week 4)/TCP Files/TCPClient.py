beb11a914bb1bea953b3aee62aa3c582554d19bd
Your Name <aryanzsingh444@gmail.com>
2026-03-20 00:01:39 +1200

    fix: typo issues

--- Lab 3 (Week 4)/TCP Files/TCPClient.py
+++ Lab 3 (Week 4)/TCP Files/TCPClient.py
@@ -2,7 +2,7 @@
 from socket import *
 
 # Address for the server
-serverName = 'localhost'
+serverName = '192.168.56.1'
 
 # Server's listening port
 serverPort = 12000
@@ -15,34 +15,34 @@ clientSocket.connect((serverName,serverPort))
 
 ## Prompt user for input
 # For python 2.x use raw_input() to get user input
-sentence = raw_input('Input lowercase sentence:')
+#sentence = raw_input('Input lowercase sentence:')
 
 # For python 3.x use input() to get user input
-#sentence = input('Input lowercase sentence:')
+sentence = input('Input lowercase sentence:')
 
 ## Send the message
 
 # For python 2.x use the following (no need to convert from unicode to byte string):
-clientSocket.send(sentence)
+#clientSocket.send(sentence)
 
 # For python 3.x use the following
 # (explicit conversion from unicode to byte string using .encode() method):
-#clientSocket.send(sentence.encode())
+clientSocket.send(sentence.encode())
 
 ## Receive the reply
 
 # For python 2.x use the following (no need to convert from byte string to unicode):
-modifiedSentence = clientSocket.recv(1024)
+#modifiedSentence = clientSocket.recv(1024)
 
 # For python 3.x use the following
 # (explicit conversion from byte string to unicode using .encode() method):
-#modifiedSentence = clientSocket.recv(1024).decode()
+modifiedSentence = clientSocket.recv(1024).decode()
 
 # For python 2.x use the following syntax for displaying strings:
-print 'From Server:', modifiedSentence
+#print ('From Server:', modifiedSentence)
 
 # For python 3.x use the following syntax for displaying strings:
-#print('From Server:', modifiedSentence)
+print('From Server:', modifiedSentence)
 
 # Close the socket
-clientSocket.close() 
+clientSocket.close()
